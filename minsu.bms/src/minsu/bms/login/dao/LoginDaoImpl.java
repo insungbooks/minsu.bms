707559631eb6660fe9b6d658096b23244747c764
soldesk <soldesk@soldesk-PC>
2017-04-12 14:47:53 +0900

    확인용

--- minsu.bms/src/minsu/bms/login/dao/LoginDaoImpl.java
+++ minsu.bms/src/minsu/bms/login/dao/LoginDaoImpl.java
@@ -1,9 +1,21 @@
 package minsu.bms.login.dao;
 
+import java.util.List;
+
 import minsu.bms.login.dao.mapper.LoginMapper;
 import minsu.bms.login.domain.User;
 
 public class LoginDaoImpl implements LoginDao {
+	
+	private String userId;
+	private String password;  
+	private String name;      
+	private int age;      
+	private String gender;
+	private String myNum;
+	private String email;     
+	private String phoneNum; 
+	private String address;
 	private LoginMapper loginMapper;
 	
 	public LoginDaoImpl(){}
@@ -14,5 +26,17 @@ public class LoginDaoImpl implements LoginDao {
 	
 	public User getUser(String id) { 
 		return loginMapper.getUser(id);
+	}
+	public int addUser(User user){
+		return loginMapper.addUser(user);
+	}
+	public int updateUser(User user){
+		return loginMapper.updateUser(user);
+	}
+	public List<User> userList(){
+		return loginMapper.userList();
+	}
+	public int delUser(User user){
+		return loginMapper.delUser(user);
 	}
 }
---
+++ minsu.bms/src/minsu/bms/login/service/SignUp.java
@@ -0,0 +1,13 @@
+package minsu.bms.login.service;
+
+import java.util.List;
+
+import minsu.bms.login.domain.User;
+
+public interface SignUp {
+	User findUser(String userId);
+	boolean join(User user);
+	boolean modifyUser(User user);
+	List<User> userList();
+	boolean deleteUser(User user);
+}
